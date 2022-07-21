b3342874cce1c33823ca955c04226d3999398764
Zehafta Berhe <zehaftatekea@gmail.com>
2022-07-21 16:33:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
+// const core = require('@actions/core');
+// const github = require('@actions/github');
+
+// try {
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+  
+//   core.setOutput("time", time);
+//   core.setOutput("name","Ze");
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
+
+import fs from 'fs';
+import { join } from 'path';
 const core = require('@actions/core');
-const github = require('@actions/github');
+
+
+const readPackageJson = function (path) {
+    const packageJson = fs.readFileSync(join(path, 'package.json')).toString();
+
+    return JSON.parse(packageJson);
+};
 
 try {
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  core.setOutput("name","Ze");
+    const path = core.getInput('path');
+
+  
+    const packageInfo = readPackageJson(path);
+
+    core.setOutput("name", packageInfo.name);
+    core.setOutput("version", packageInfo.version);
+    core.setOutput("npmjs-link", `https://www.npmjs.com/package/${packageInfo.name}`);
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
